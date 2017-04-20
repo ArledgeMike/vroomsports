c0cb6c469971adedbf4305136068903c708fd8c5
marledge <marledge@cars.com>
2017-04-19 22:30:18 -0500

    added proper logo to home page

--- app/index.js
+++ app/index.js
@@ -2,3 +2,4 @@ import './public/scss/styles.scss';
 
 var favicon =  require('./public/images/favicon.ico');
 var logo = require('./public/images/rf_logo.png');
+var vroomLogo = require('./public/images/vroom_logo.png');
